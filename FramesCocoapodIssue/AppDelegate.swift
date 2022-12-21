41de42e10c13362c9559b8364f6d9110f87c8ae6
rock88 <rock88a@gmail.com>
2022-12-21 17:08:28 +0300

    Add sources

---
+++ FramesCocoapodIssue/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.rootViewController = UIViewController()
+        window?.rootViewController?.view.backgroundColor = .white
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '14.0'
+
+target 'FramesCocoapodIssue' do
+  # Build Succeed
+  #pod 'Frames', '4.0.1'
+
+  # Build Failed: Multiple commands produce 'FramesCocoapodIssue.app/Assets.car'
+  pod 'Frames', '4.0.3'
+end
